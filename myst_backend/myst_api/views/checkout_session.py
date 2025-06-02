4d66f5a6d6f4296bc3dca5237ff5e30119bcc012
Izaak Ford-Dow <rillastudios@outlook.com>
2025-06-02 01:05:54 -0300

    Added robot.txt and sitemap.xml.gz to the public folder, added address model, serializer, and service, added shipping companies enum, added order service, added exchange view, added order view, added review view, removed old migrations and images, updated checkout session view, updated order serializer, updated price conversion service.

--- myst_backend/myst_api/views/checkout_session.py
+++ myst_backend/myst_api/views/checkout_session.py
@@ -4,17 +4,26 @@ from rest_framework import status
 import stripe
 from django.conf import settings
 from myst_api.models.product import Product
+from myst_api.service.price_conversion import convert_price
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 class CreateCheckoutSessionView(APIView):
-    def get(self, request, session_id):
+    def get(self, request):
+
+        session_id = request.query_params.get('session_id')
+        if not session_id:
+            return Response({'error': 'session_id is required'}, status=status.HTTP_400_BAD_REQUEST)
+
         try:
             session = stripe.checkout.Session.retrieve(
                 session_id,
                 expand=["total_details"]
             )
-            return Response(session, status=status.HTTP_200_OK)
+
+            session_items = stripe.checkout.Session.list_line_items(
+                session_id)
+            return Response({"session": session, "items": session_items}, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -23,47 +32,41 @@ class CreateCheckoutSessionView(APIView):
             # Assume the request contains a product_id and quantity
             product_id = request.data.get('product_id')
             quantity = request.data.get('quantity', 1)
+            currency = request.data.get('currency')
 
             # Fetch the product from your database
             product = Product.objects.get(product_id=product_id)
 
-            # Calculate the price (this logic would depend on your pricing model)
-            unit_amount = int(product.product_price * 100)  # Stripe expects amounts in cents
+            updated_price = convert_price(str(currency).upper(), float(product.product_price))
+
+            unit_amount = int(updated_price * 100)
 
             session = stripe.checkout.Session.create(
                 line_items=[
                     {
                         'price_data': {
-                            'currency': 'cad',
+                            'currency': str(currency.lower()),
                             'unit_amount': unit_amount,
                             'product_data': {
                                 'name': product.product_name,
                                 'description': product.product_description,
+                                'tax_code': 'txcd_99999999',
                             },
                         },
                         'quantity': quantity,
                     },
                 ],
+                adaptive_pricing={"enabled": False},
+                automatic_tax={'enabled': False},
                 mode='payment',
-                automatic_tax={'enabled': True},
                 shipping_address_collection={
-                    'allowed_countries': ['CA', 'US'],  # Adjust as needed
+                    'allowed_countries': ['CA', 'US'],
                 },
                 ui_mode='custom',
-                return_url='http://localhost:5173/return?session_id={CHECKOUT_SESSION_ID}',  # Replace with your success URL
-            )
+          )
 
             return Response({'clientSecret': session.client_secret}, status=status.HTTP_201_CREATED)
         except Product.DoesNotExist:
             return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
-
-
-class CheckoutSessionStatusView(APIView):
-    def get(self, request, session_id):
-        try:
-            session = stripe.checkout.Session.retrieve(session_id)
-            return Response({'status': session.status, 'customer_email': session.customer.email}, status=status.HTTP_200_OK)
-        except stripe.error.StripeError as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
