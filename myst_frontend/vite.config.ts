a010e38268057fb47b5332b6c5c4e111369950d5
Izaak Ford-Dow <rillastudios@outlook.com>
2025-06-09 16:47:43 -0300

    Finalized vite rollup for myst frontend.

--- myst_frontend/vite.config.ts
+++ myst_frontend/vite.config.ts
@@ -8,10 +8,25 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          'vendor-react': ['react', 'react-dom'],
-          'vendor-ui': ['@mantine/core', '@mantine/hooks'],
+          'vendor-react': ['react', 'react-dom', 'react-helmet-async'],
+
+          'vendor-mantine': [
+            '@mantine/core',
+            '@mantine/hooks',
+            '@mantine/notifications',
+            '@mantine/form',
+            '@tabler/icons-react'
+          ],
+          'vendor-carousel': [
+            'embla-carousel',
+            'embla-carousel-react',
+            'embla-carousel-autoplay',
+            'embla-carousel-auto-scroll'
+          ],
+          'vendor-router': ['react-router-dom'],
+          'vendor-stripe': ['@stripe/react-stripe-js', '@stripe/stripe-js']
         }
       }
-    }
+    },
   }
 })
